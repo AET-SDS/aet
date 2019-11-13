cbbdc030e5a6822a41c63ec51ed89c0a810f3aeb
Dominik Grzelak <grzelak.dominik@gmail.com>
2019-11-13 22:20:11 +0100

    [SDSAET-34] Refactor GrouperDispatchers

--- core/worker/src/main/java/com/cognifide/aet/worker/impl/GrouperDispatcherImpl.java
+++ core/worker/src/main/java/com/cognifide/aet/worker/impl/GrouperDispatcherImpl.java
@@ -4,62 +4,33 @@ import com.cognifide.aet.communication.api.JobStatus;
 import com.cognifide.aet.communication.api.job.GrouperJobData;
 import com.cognifide.aet.communication.api.job.GrouperResultData;
 import com.cognifide.aet.communication.api.metadata.Comparator;
-import com.cognifide.aet.job.api.grouper.GrouperFactory;
 import com.cognifide.aet.job.api.grouper.GrouperJob;
 import com.cognifide.aet.worker.api.GrouperDispatcher;
-import com.cognifide.aet.worker.api.JobRegistry;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 class GrouperDispatcherImpl implements GrouperDispatcher {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(GrouperDispatcherImpl.class);
+  private final Map<Comparator, AtomicLong> comparatorCounts;
+  private final Map<Comparator, GrouperJob> grouperJobs;
 
-  private final Map<Comparator, AtomicLong> comparatorCounts = new HashMap<>();
-  private final Map<Comparator, GrouperJob> grouperJobs = new HashMap<>();
-  private final AtomicInteger counter = new AtomicInteger(0);
-  private final int numberOfComparatorTypes;
-
-  //todo clean this mess
-  GrouperDispatcherImpl(JobRegistry jobRegistry, Map<Comparator, Long> comparatorCounts) {
-    comparatorCounts.forEach((key, value) -> this.comparatorCounts.put(key, new AtomicLong(value)));
-    this.numberOfComparatorTypes = comparatorCounts.keySet().size();
-    for (Comparator comparator : comparatorCounts.keySet()) {
-      String comparatorTypeName = comparator.getType();
-      Optional<GrouperFactory> grouperFactory = jobRegistry.getGrouperFactory(comparatorTypeName);
-      if (grouperFactory.isPresent()) {
-        GrouperJob grouperJob =
-            grouperFactory.get().createInstance(comparatorCounts.get(comparator));
-        grouperJobs.put(comparator, grouperJob);
-      } else {
-        LOGGER.warn("GrouperJob not found for given type: {}", comparatorTypeName);
-      }
-    }
+  GrouperDispatcherImpl(
+      Map<Comparator, AtomicLong> comparatorCounts, Map<Comparator, GrouperJob> grouperJobs) {
+    this.comparatorCounts = comparatorCounts;
+    this.grouperJobs = grouperJobs;
   }
 
   @Override
   public GrouperResultData run(String correlationId, GrouperJobData grouperJobData) {
     Comparator comparisonResult = grouperJobData.getComparisonResult();
     GrouperResultData result;
-    if (!grouperJobs.containsKey(comparisonResult)) {   //todo improve
+    if (!grouperJobs.containsKey(comparisonResult)) {
       long value = comparatorCounts.get(comparisonResult).decrementAndGet();
       result = new GrouperResultData(JobStatus.SUCCESS, value == 0);
     } else {
       GrouperJob grouperJob = grouperJobs.get(comparisonResult);
       result = grouperJob.group(grouperJobData);
     }
-    if (result.isReady()) {
-      counter.incrementAndGet();
-    }
     return result;
-  }
-
-  boolean isFinished() {
-    return counter.get() == numberOfComparatorTypes; // todo move to wrapper?, enhance?
   }
 }
--- core/worker/src/main/java/com/cognifide/aet/worker/impl/GrouperDispatcherWrapper.java
+++ core/worker/src/main/java/com/cognifide/aet/worker/impl/GrouperDispatcherWrapper.java
@@ -2,10 +2,20 @@ package com.cognifide.aet.worker.impl;
 
 import com.cognifide.aet.communication.api.job.GrouperJobData;
 import com.cognifide.aet.communication.api.job.GrouperResultData;
+import com.cognifide.aet.communication.api.metadata.Comparator;
+import com.cognifide.aet.job.api.grouper.GrouperFactory;
+import com.cognifide.aet.job.api.grouper.GrouperJob;
 import com.cognifide.aet.worker.api.GrouperDispatcher;
 import com.cognifide.aet.worker.api.JobRegistry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
@@ -16,23 +26,65 @@ public class GrouperDispatcherWrapper implements GrouperDispatcher {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GrouperDispatcherWrapper.class);
 
-  @Reference
-  private JobRegistry jobRegistry;
+  @Reference private JobRegistry jobRegistry;
 
-  private final ConcurrentMap<String, GrouperDispatcherImpl> dispatchers =
+  private final ConcurrentMap<String, AtomicInteger> completedJobsCounters =
       new ConcurrentHashMap<>();
+  private final ConcurrentMap<String, GrouperDispatcher> dispatchers = new ConcurrentHashMap<>();
 
   @Override
   public GrouperResultData run(String correlationId, GrouperJobData grouperJobData) {
-    GrouperDispatcherImpl dispatcher =
-        dispatchers.computeIfAbsent(
-            correlationId,
-            it -> new GrouperDispatcherImpl(jobRegistry, grouperJobData.getComparatorCounts()));
+    Map<Comparator, Long> comparatorCounts = grouperJobData.getComparatorCounts();
+    GrouperDispatcher dispatcher = getDispatcher(correlationId, comparatorCounts);
     GrouperResultData resultData = dispatcher.run(correlationId, grouperJobData);
-    if (dispatcher.isFinished()) {
-      LOGGER.error("DELETING DISPATCHER FOR ID: {}", correlationId);
-      dispatchers.remove(correlationId);
+    if (resultData.isReady()) {
+      AtomicInteger completedJobsCounter = getCompletedJobsCounter(correlationId, comparatorCounts);
+      int numberOfPendingJobs = completedJobsCounter.decrementAndGet();
+      if (numberOfPendingJobs == 0) {
+        LOGGER.error("DELETING DISPATCHER FOR ID: {}", correlationId); // todo
+        dispatchers.remove(correlationId);
+        completedJobsCounters.remove(correlationId);
+      }
     }
     return resultData;
+  }
+
+  private AtomicInteger getCompletedJobsCounter(
+      String correlationId, Map<Comparator, Long> comparatorCounts) {
+    return completedJobsCounters.computeIfAbsent(
+        correlationId, __ -> new AtomicInteger(comparatorCounts.keySet().size()));
+  }
+
+  private GrouperDispatcher getDispatcher(
+      String correlationId, Map<Comparator, Long> comparatorCounts) {
+    return dispatchers.computeIfAbsent(
+        correlationId,
+        __ -> {
+          Map<Comparator, AtomicLong> counters = prepareComparatorCounters(comparatorCounts);
+          Map<Comparator, GrouperJob> grouperJobs = prepareGrouperJobs(comparatorCounts);
+          return new GrouperDispatcherImpl(counters, grouperJobs);
+        });
+  }
+
+  private Map<Comparator, AtomicLong> prepareComparatorCounters(
+      Map<Comparator, Long> comparatorCounts) {
+    return comparatorCounts.entrySet().stream()
+        .collect(Collectors.toMap(Entry::getKey, it -> new AtomicLong(it.getValue())));
+  }
+
+  private Map<Comparator, GrouperJob> prepareGrouperJobs(Map<Comparator, Long> comparatorCounts) {
+    Map<Comparator, GrouperJob> grouperJobs = new HashMap<>();
+    for (Comparator comparator : comparatorCounts.keySet()) {
+      String comparatorTypeName = comparator.getType();
+      Optional<GrouperFactory> grouperFactory = jobRegistry.getGrouperFactory(comparatorTypeName);
+      if (grouperFactory.isPresent()) {
+        long expectedInputCount = comparatorCounts.get(comparator);
+        GrouperJob grouperJob = grouperFactory.get().createInstance(expectedInputCount);
+        grouperJobs.put(comparator, grouperJob);
+      } else {
+        LOGGER.warn("GrouperJob not found for given type: {}", comparatorTypeName);
+      }
+    }
+    return grouperJobs;
   }
 }
