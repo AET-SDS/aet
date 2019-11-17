86ff1db2acef475921dae5c15d39a1d4ebcba3e0
Dominik Grzelak <grzelak.dominik@gmail.com>
2019-11-17 11:06:33 +0100

    [SDSAET-34] Compare every jsError and calculate distances

--- core/jobs/src/main/java/com/cognifide/aet/job/common/groupers/jserrors/JsErrorsGrouper.java
+++ core/jobs/src/main/java/com/cognifide/aet/job/common/groupers/jserrors/JsErrorsGrouper.java
@@ -8,39 +8,74 @@ import com.cognifide.aet.job.api.collector.JsErrorLog;
 import com.cognifide.aet.job.api.grouper.GrouperJob;
 import com.cognifide.aet.vs.ArtifactsDAO;
 import com.cognifide.aet.vs.DBKey;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
 import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsErrorsGrouper implements GrouperJob {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(JsErrorsGrouper.class);
   private static final Type COMPARATOR_OUTPUT_TYPE = new TypeToken<Set<JsErrorLog>>() {}.getType();
 
   private final ArtifactsDAO artifactsDAO;
   private final DBKey dbKey;
   private final AtomicLong inputCounter;
+  private final Multimap<String, JsErrorLog> jsErrors = LinkedListMultimap.create();
+  private final Map<Pair<Entry<String, JsErrorLog>, Entry<String, JsErrorLog>>, Integer> distances;
 
   JsErrorsGrouper(ArtifactsDAO artifactsDAO, DBKey dbKey, AtomicLong inputCounter) {
     this.artifactsDAO = artifactsDAO;
     this.dbKey = dbKey;
     this.inputCounter = inputCounter;
+    this.distances = new HashMap<>();
   }
 
   @Override
   public GrouperResultData group(GrouperJobData jobData) {
-    Comparator comparisonResult = jobData.getComparisonResult();
-    String artifactId = comparisonResult.getStepResult().getArtifactId();
     try {
+      String urlName = jobData.getUrlName();
+      Comparator comparisonResult = jobData.getComparisonResult();
+      String artifactId = comparisonResult.getStepResult().getArtifactId();
       Set<JsErrorLog> jsErrorLogs =
-          artifactsDAO.getJsonFormatArtifact(dbKey, artifactId, COMPARATOR_OUTPUT_TYPE);
-      String a = "";
+          artifactsDAO.getJsonFormatArtifact(
+              dbKey, artifactId, COMPARATOR_OUTPUT_TYPE); // todo entries will contain urls
+      jsErrorLogs.forEach(it -> jsErrors.put(urlName, it));
     } catch (IOException e) {
       e.printStackTrace(); // todo
     }
 
     long value = inputCounter.decrementAndGet();
+    if (value == 0) {
+      calculateDistances();
+      String artifactId = artifactsDAO.saveArtifactInJsonFormat(dbKey, distances);
+      // todo prints:
+      // "(promocje\u003dcom.cognifide.aet.job.api.collector.JsErrorLog@90ac2780,promocje\u003dcom.cognifide.aet.job.api.collector.JsErrorLog@90ac2780)":0
+      LOGGER.error("GROUPER RESULT ARTIFACT_ID: {}", artifactId); // todo
+    }
     return new GrouperResultData(JobStatus.SUCCESS, value == 0);
+  }
+
+  private void calculateDistances() {
+    for (Entry<String, JsErrorLog> entry1 : jsErrors.entries()) {
+      for (Entry<String, JsErrorLog> entry2 : jsErrors.entries()) {
+        // todo what if the same
+        int distance =
+            new LevenshteinDistance()
+                .apply(entry1.getValue().getErrorMessage(), entry2.getValue().getErrorMessage());
+        Pair<Entry<String, JsErrorLog>, Entry<String, JsErrorLog>> key = Pair.of(entry1, entry2);
+        distances.put(key, distance);
+      }
+    }
   }
 }
