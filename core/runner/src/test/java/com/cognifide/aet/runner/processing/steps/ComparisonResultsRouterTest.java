44d2cbbb6acf84ac2a2a7f2cf38e8fad4f5352d5
Dominik Grzelak <grzelak.dominik@gmail.com>
2019-12-02 22:41:29 +0100

    [SDSAET-34] Fix broken test

--- core/runner/src/test/java/com/cognifide/aet/runner/processing/steps/ComparisonResultsRouterTest.java
+++ core/runner/src/test/java/com/cognifide/aet/runner/processing/steps/ComparisonResultsRouterTest.java
@@ -17,7 +17,6 @@ package com.cognifide.aet.runner.processing.steps;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
@@ -26,10 +25,11 @@ import static org.mockito.Mockito.when;
 
 import com.cognifide.aet.communication.api.JobStatus;
 import com.cognifide.aet.communication.api.job.ComparatorResultData;
+import com.cognifide.aet.communication.api.messages.ProgressLog;
+import com.cognifide.aet.communication.api.metadata.Comparator;
 import com.cognifide.aet.communication.api.metadata.Step;
 import com.cognifide.aet.communication.api.metadata.Suite.Timestamp;
 import com.cognifide.aet.communication.api.metadata.Url;
-import com.cognifide.aet.communication.api.messages.ProgressLog;
 import com.google.common.collect.Lists;
 import java.util.Collections;
 import java.util.Observable;
@@ -73,12 +73,13 @@ public class ComparisonResultsRouterTest extends StepManagerTest {
     when(mockedMessage.getObject()).thenReturn(comparatorResultData);
     when(comparatorResultData.getStatus()).thenReturn(JobStatus.SUCCESS);
     when(comparatorResultData.getStepIndex()).thenReturn(0);
+    when(comparatorResultData.getComparisonResult()).thenReturn(new Comparator("type"));
     Url mockedUrl = Mockito.mock(Url.class);
     when(mockedUrl.getSteps()).thenReturn(Collections.singletonList(Mockito.mock(Step.class)));
     when(runIndexWrapper.getTestUrl(anyString(), anyString())).thenReturn(Optional.of(mockedUrl));
     ((ComparisonResultsRouter) tested).updateAmountToReceive(1);
-    when(runIndexWrapper.get().getRealSuite()).thenReturn(suite);//todo check if needed still
-    when(suite.getTests()).thenReturn(Lists.newArrayList());//todo check if needed still
+    when(runIndexWrapper.get().getRealSuite()).thenReturn(suite);
+    when(suite.getTests()).thenReturn(Lists.newArrayList());
 
     ProgressLog progress = tested.getProgress();
     assertThat(progress.toString(), is("COMPARED: [success:   0, total:   1]"));
@@ -93,12 +94,13 @@ public class ComparisonResultsRouterTest extends StepManagerTest {
     when(mockedMessage.getObject()).thenReturn(comparatorResultData);
     when(comparatorResultData.getStatus()).thenReturn(JobStatus.ERROR);
     when(comparatorResultData.getStepIndex()).thenReturn(0);
+    when(comparatorResultData.getComparisonResult()).thenReturn(new Comparator("type"));
     Url mockedUrl = Mockito.mock(Url.class);
     when(mockedUrl.getSteps()).thenReturn(Collections.singletonList(Mockito.mock(Step.class)));
     when(runIndexWrapper.getTestUrl(anyString(), anyString())).thenReturn(Optional.of(mockedUrl));
     ((ComparisonResultsRouter) tested).updateAmountToReceive(1);
-    when(runIndexWrapper.get().getRealSuite()).thenReturn(suite);//todo check if needed still
-    when(suite.getTests()).thenReturn(Lists.newArrayList());//todo check if needed still
+    when(runIndexWrapper.get().getRealSuite()).thenReturn(suite);
+    when(suite.getTests()).thenReturn(Lists.newArrayList());
 
     ProgressLog progress = tested.getProgress();
     assertThat(progress.toString(), is("COMPARED: [success:   0, total:   1]"));
