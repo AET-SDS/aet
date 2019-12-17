fa319ef576d17e333635574a85f2e22ef9fa69cc
Patryk <dolata.patryk1@gmail.com>
2019-12-17 18:19:06 +0100

    Process of each error group moved from GroupsService to new class GroupsFactory

--- rest-endpoint/src/main/java/com/cognifide/aet/services/GroupsService.java
+++ rest-endpoint/src/main/java/com/cognifide/aet/services/GroupsService.java
@@ -16,16 +16,11 @@
 package com.cognifide.aet.services;
 
 import com.cognifide.aet.communication.api.metadata.Test;
-import com.cognifide.aet.job.api.collector.JsErrorLog;
+import com.cognifide.aet.factories.GroupsFactory;
 import com.cognifide.aet.models.ErrorType;
-import com.cognifide.aet.models.Groups;
-import com.cognifide.aet.vs.ArtifactsDAO;
 import com.cognifide.aet.vs.DBKey;
 import com.google.common.base.Strings;
-import com.google.gson.reflect.TypeToken;
-import java.io.IOException;
 import java.io.Serializable;
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -36,13 +31,10 @@ import org.osgi.service.component.annotations.Reference;
 @Component(service = GroupsService.class, immediate = true)
 public class GroupsService implements Serializable {
 
-  private static final Type JS_ERRORS_GROUPS_TYPE = new TypeToken<Set<Set<JsErrorLog>>>() {
-  }.getType();
-
   private static final long serialVersionUID = -1177482293065252166L;
 
   @Reference
-  private ArtifactsDAO artifactsDAO;
+  private GroupsFactory groupsFactory;
 
   public Map<ErrorType, Set<Set>> getGroupsFromTest(Test test, DBKey dbKey,
       String errorType) {
@@ -52,29 +44,15 @@ public class GroupsService implements Serializable {
       String artifactId = test.getGrouperResults().get(errorType);
       if (!Strings.isNullOrEmpty(artifactId)) {
         ErrorType type = ErrorType.byStringType(errorType);
-        groupsMap.put(type, processGroups(type, artifactId, dbKey));
+        groupsMap.put(type, groupsFactory.processGroups(type, dbKey, artifactId));
       }
     } else {
       for (Entry<String, String> entry : test.getGrouperResults().entrySet()) {
         ErrorType type = ErrorType.byStringType(entry.getKey());
-        groupsMap.put(type, processGroups(type, entry.getValue(), dbKey));
+        groupsMap.put(type, groupsFactory.processGroups(type, dbKey, entry.getValue()));
       }
     }
 
     return groupsMap;
-  }
-
-  public Set<Set> processGroups(ErrorType errorType, String artifactId, DBKey dbKey) {
-    try {
-      switch (errorType) {
-        case JS_ERRORS:
-          return artifactsDAO.getJsonFormatArtifact(dbKey, artifactId,
-              JS_ERRORS_GROUPS_TYPE);
-        default:
-          throw new IllegalArgumentException();
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
